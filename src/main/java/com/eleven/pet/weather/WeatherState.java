ac2eaaf5a12c11aea6604230e6817d1c18d5bd15
heim1024 <melwin.heimby@gmail.com>
2025-11-18 16:03:51 +0100

    WeatherState

---
+++ src/main/java/com/eleven/pet/weather/WeatherState.java
@@ -0,0 +1,6 @@
+package com.eleven.pet.weather;
+
+public interface WeatherState {
+    public String getName();
+    public double getHappinessModifier();
+}
--- src/main/java/com/eleven/pet/weather/WeatherSystem.java
+++ src/main/java/com/eleven/pet/weather/WeatherSystem.java
@@ -1,4 +1,31 @@
 package com.eleven.pet.weather;
 
-public class WeatherSystem {
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.ReadOnlyObjectProperty;
+
+public class WeatherSystem implements WeatherState {
+    private ObjectProperty<WeatherState> currentState;
+
+    public void updateWeather(){
+
+    }
+
+    public WeatherState getCurrentState(){
+        WeatherState state = null;
+        return state;
+    }
+    public ReadOnlyObjectProperty<WeatherState> getCurrentWeatherProperty(){
+        WeatherState state = null;
+        return (ReadOnlyObjectProperty<WeatherState>) state;
+    }
+
+    @Override
+    public String getName() {
+        return "";
+    }
+
+    @Override
+    public double getHappinessModifier() {
+        return 0;
+    }
 }
