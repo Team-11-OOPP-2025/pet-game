0f0fcdfc666773280b23cac70147deaaf5645425
Anajrim <itroublve99@gmail.com>
2025-11-18 16:26:15 +0100

    update work env jdk

--- src/main/java/com/eleven/pet/MainApp.java
+++ src/main/java/com/eleven/pet/MainApp.java
@@ -1,6 +1,13 @@
 package com.eleven.pet;
 
+import javafx.stage.Stage;
+
 public class MainApp {
+
+    public void start(Stage stage) {
+        // Application entry point
+    }
+
     public static void main(String[] args) {
         // do something
     }
