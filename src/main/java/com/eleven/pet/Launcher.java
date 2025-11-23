c97e796f6d9d7d1213b8f139a0dd559dbbaaaea0
Anajrim <itroublve99@gmail.com>
2025-11-23 07:21:07 +0100

    Fix formatting

--- src/main/java/com/eleven/pet/Launcher.java
+++ src/main/java/com/eleven/pet/Launcher.java
@@ -1,6 +1,6 @@
 package com.eleven.pet;
 
-public class  Launcher {
+public class Launcher {
     public static void main(String[] args) {
         MainApp.initializeApplication(args);
     }
--- src/main/java/com/eleven/pet/MainApp.java
+++ src/main/java/com/eleven/pet/MainApp.java
@@ -10,26 +10,26 @@ import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class MainApp extends Application {
-    
+
     @Override
     public void start(Stage primaryStage) {
         // Create Model
         PetModel model = new PetModel("MyPet");
-        
+
         // Create Controller
         PetController controller = new PetController(model, null);
-        
+
         // Create View
         PetView view = new PetView(model, controller);
         Pane root = view.initializeUI();
-        
+
         // Setup Scene and Stage
         Scene scene = new Scene(root, 800, 600);
         primaryStage.setTitle("Pet Game");
         primaryStage.setScene(scene);
         primaryStage.setResizable(true);
         primaryStage.show();
-        
+
         // Start game loop
         controller.startGameLoop();
     }
