b8f27ed3d199aed8bf196b37827d712773475358
Anajrim <itroublve99@gmail.com>
2025-11-28 07:21:41 +0100

    Add unit tests for AssetLoader and include JUnit dependencies
    Signed-off-by: Anajrim <itroublve99@gmail.com>

---
+++ src/test/java/AssetLoaderTests.java
@@ -0,0 +1,32 @@
+import com.eleven.pet.view.AssetLoader;
+import javafx.scene.image.Image;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
+
+public class AssetLoaderTests {
+    @Test
+    public void testAssetLoading() {
+        AssetLoader assetLoader = AssetLoader.getInstance();
+        String imageName = "Bear";
+        Image image = assetLoader.getImage(imageName);
+        assertEquals(311.0, image.getWidth(), 0.1);
+        assertEquals(461.0, image.getHeight(), 0.1);
+    }
+
+    @Test
+    public void testSingleton() {
+        AssetLoader assetLoader = AssetLoader.getInstance();
+        AssetLoader anotherInstance = AssetLoader.getInstance();
+        assertSame(assetLoader, anotherInstance);
+    }
+
+    @Test
+    public void testMissingImageFallback() {
+        AssetLoader assetLoader = AssetLoader.getInstance();
+        String missingImagePath = "images/non_existent_image.png";
+        Image image = assetLoader.getImage(missingImagePath);
+        assertSame(null, image);
+    }
+}
