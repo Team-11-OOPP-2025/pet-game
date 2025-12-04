790f3ad1eb6bf56aeff3dd576291b822f8f4a33f
Anajrim <itroublve99@gmail.com>
2025-12-04 12:37:29 +0100

    test: refactor InventoryTest to initialize inventory in setup method
    Signed-off-by: Anajrim <itroublve99@gmail.com>

--- src/test/java/InventoryTest.java
+++ src/test/java/InventoryTest.java
@@ -1,14 +1,21 @@
 import com.eleven.pet.model.Inventory;
 import com.eleven.pet.model.items.FoodItem;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class InventoryTest {
 
+    Inventory inventory;
+
+    @BeforeEach
+    void setUp() {
+        inventory = new Inventory();
+    }
+
     @Test
     void testAddItem() {
-        Inventory inventory = new Inventory();
         FoodItem apple = new FoodItem("Apple", 10);
         inventory.add(apple, 1);
 
@@ -17,7 +24,6 @@ public class InventoryTest {
 
     @Test
     void testRemoveItem() {
-        Inventory inventory = new Inventory();
         FoodItem banana = new FoodItem("Banana", 15);
         inventory.add(banana, 2);
         inventory.remove(banana, 1);
